366d460086497f8f8bea41fc13af3651f0657d33
gauravdoc97 <gauravdoc97@gmail.com>
2022-05-25 00:40:28 +0530

    footer added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,8 @@ const Footer = () => {
   return (
     <>
       <footer className="w-100  bg-transparent text-center  p-2">
-        {/* <p>© 2021 SubhamTech. All Rights Reserved</p> */}
+        {/* <p class="mb-0">© 2022  All Rights Reserved</p> */}
+        <p><abbr title="attribute">© 2022  All Rights Reserved</abbr></p>
       </footer>
     </>
   );
